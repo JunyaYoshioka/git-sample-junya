984d13a9b1c0764362322c97f0549acb2169f824
junyayoshioka <yoshiokajunya@yoshiokajunyanoMacBook-Pro.local>
2019-08-01 15:14:15 +0900

    Carクラス作成したよ

---
+++ src/main/java/com/example/demo/controller/Car.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+public class Car {
+	private int speed1;
+	private int speed2;
+	
+
+	public int getSpeed1() {
+		return speed1;
+	}
+
+	public void setSpeed1(int speed1) {
+		this.speed1 = speed1;
+	}
+
+	public int getSpeed2() {
+		return speed2;
+	}
+
+	public void setSpeed2(int speed2) {
+		this.speed2 = speed2;
+	}
+
+	
+}
